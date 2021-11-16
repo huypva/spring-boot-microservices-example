bc67be16cede07aa982b4c975d9fac81da4713e0
huypva <huypva@vng.com.vn>
2021-11-16 14:49:24 +0700

    Add mysql

---
+++ infrastucture/database/init.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS `service_c`;
+
+USE service_c;
+CREATE TABLE `message_c` (
+  message_id INT(11) NOT NULL,
+  message VARCHAR(50) NOT NULL,
+  PRIMARY KEY (message_id)
+);
+
+INSERT INTO `message_c` VALUES (1, "abc");
+INSERT INTO `message_c` VALUES (2, "def");
