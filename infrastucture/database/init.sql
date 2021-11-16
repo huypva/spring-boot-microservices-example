5e46037067d147593758096c0acc3abcf1fd2bba
huypva <huypva@vng.com.vn>
2021-11-16 15:40:19 +0700

    Update init db

--- infrastucture/database/init.sql
+++ infrastucture/database/init.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS `service_c`;
 
 USE service_c;
 CREATE TABLE `message_c` (
-  message_id INT(11) NOT NULL,
+  message_id INT(11) NOT NULL DEFAULT 0,
   message VARCHAR(50) NOT NULL,
   PRIMARY KEY (message_id)
 );
