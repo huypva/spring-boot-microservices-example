bcd06bd4a63012ac89b890f34a0b121461e28f9d
huypva <huypva@vng.com.vn>
2021-11-17 10:37:23 +0700

    Add log

--- service-C/src/main/java/io/codebyexample/servicec/core/entity/MessageC.java
+++ service-C/src/main/java/io/codebyexample/servicec/core/entity/MessageC.java
@@ -1,7 +1,9 @@
 package io.codebyexample.servicec.core.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 /**
@@ -10,6 +12,8 @@ import lombok.Setter;
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class MessageC {
 
   int id;
--- service-C/src/main/java/io/codebyexample/servicec/dataprovider/messagec/MessageCDataProviderImpl.java
+++ service-C/src/main/java/io/codebyexample/servicec/dataprovider/messagec/MessageCDataProviderImpl.java
@@ -4,13 +4,16 @@ import io.codebyexample.servicec.core.entity.MessageC;
 import io.codebyexample.servicec.dataprovider.messagec.jpa.MessageCDTO;
 import io.codebyexample.servicec.dataprovider.messagec.jpa.MessageCRepository;
 import io.codebyexample.servicec.dataprovider.messagec.redis.MessageCRedisClient;
+import io.codebyexample.servicec.util.GsonUtils;
 import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
  * @author huypva
  */
+@Slf4j
 @Component
 public class MessageCDataProviderImpl implements MessageCDataProvider {
 
@@ -22,7 +25,11 @@ public class MessageCDataProviderImpl implements MessageCDataProvider {
 
   @Override
   public void setMessageC(MessageC messageC) {
+
+    log.info("Redis.setMessageC: " + GsonUtils.toJson(messageC));
     messageCRedisClient.setMessageC(messageC);
+
+    log.info("Database.save: " + GsonUtils.toJson(messageC));
     MessageCDTO messageCDto = new MessageCDTO(messageC.getId(), messageC.getMessage());
     messageCRepository.save(messageCDto);
   }
@@ -30,11 +37,13 @@ public class MessageCDataProviderImpl implements MessageCDataProvider {
   @Override
   public MessageC getMessageC(int id) {
     MessageC messageC = messageCRedisClient.getMessageC(id);
+    log.info("Redis.getMessageC: " + GsonUtils.toJson(messageC));
     if (messageC != null) {
       return messageC;
     }
 
     Optional<MessageCDTO> optionalMessageCDTO = messageCRepository.findById(id);
+    log.info("Database.getMessageC: " + GsonUtils.toJson(optionalMessageCDTO.orElse(null)));
     if (optionalMessageCDTO.isPresent()) {
       MessageCDTO messageCDTO = optionalMessageCDTO.get();
       messageC = MessageC.builder()
