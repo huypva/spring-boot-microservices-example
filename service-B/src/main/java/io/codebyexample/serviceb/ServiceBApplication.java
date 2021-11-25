e10b273de05342792811f5a18663fcbe74387804
huypva <huypva@vng.com.vn>
2021-11-25 10:08:29 +0700

    Format config

--- service-B/src/main/java/io/codebyexample/serviceb/ServiceBApplication.java
+++ service-B/src/main/java/io/codebyexample/serviceb/ServiceBApplication.java
@@ -1,11 +1,7 @@
 package io.codebyexample.serviceb;
 
-import java.util.Arrays;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ServiceBApplication {
@@ -14,18 +10,5 @@ public class ServiceBApplication {
         SpringApplication.run(ServiceBApplication.class, args);
     }
 
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
-
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
-        };
-    }
 }
 
